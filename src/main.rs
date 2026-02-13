088370dbf276a46aad9b29a7ec255108bcc7baac
abhishekce17 <abhisheklprajapati18@gmail.com>
2026-02-13 22:05:23 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,16 @@ fn main() {
     // let mut followup : String = String::new();
     loop {
         command.clear();
-        print!("\n$ ");
+        print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).unwrap();
-        if command.trim() == "exit" {break;}
+        if command.trim().len() == 0 {continue;}
+        else if command.trim() == "exit" {break;}
         else if command[0..4].trim() == "echo" {
-            print!("{}", command[4..].trim());
-            io::stdout().flush().unwrap();
+            print!("{}\n", command[4..].trim());
         } 
         else {
-            print!("{}: command not found", command.trim());
-            io::stdout().flush().unwrap();
+            print!("{}: command not found\n", command.trim());
         }
     }
 }
