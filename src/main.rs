5db320af3b8851403bb1174ad711fb52071667d3
abhishekce17 <abhisheklprajapati18@gmail.com>
2026-02-13 21:11:07 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     let mut command : String = String::new();
+    // let mut followup : String = String::new();
     loop {
         command.clear();
-        print!("$ ");
+        print!("\n$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).unwrap();
-        if command.trim() == "exit" {break;};
-        println!("{}: command not found", command.trim());
+        if command.trim() == "exit" {break;}
+        else if command[0..4].trim() == "echo" {
+            print!("{}", command[4..].trim());
+        } 
+        else {
+            print!("{}: command not found", command.trim());
+        }
     }
 }
